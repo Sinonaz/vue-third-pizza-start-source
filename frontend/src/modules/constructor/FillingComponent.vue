b0fe29b6f0357a4eefe9daa5d5e501a565fca151
Isaac <53692366+Sinonaz@users.noreply.github.com>
2025-03-19 16:39:44 +0200

    Разбивка элементов на компоненты

---
+++ frontend/src/modules/constructor/FillingComponent.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { getImage } from "@/common/helpers";
+import CounterComponent from "@/common/components/CounterComponent.vue";
+
+defineProps({
+  ingredientItems: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="ingredients__filling">
+    <p>Начинка:</p>
+
+    <ul class="ingredients__list">
+      <li
+        v-for="ingredient in ingredientItems"
+        :key="ingredient.id"
+        class="ingredients__item"
+      >
+        <span class="filling filling--mushrooms">
+          <img :src="getImage(ingredient.image)" :alt="ingredient.name" />
+          {{ ingredient.name }}
+        </span>
+
+        <counter-component :counter-value="0" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/ds-system/ds.scss";
+@import "@/assets/scss/mixins/mixins.scss";
+
+.ingredients__filling {
+  width: 100%;
+
+  p {
+    @include r-s16-h19;
+
+    margin-top: 0;
+    margin-bottom: 16px;
+  }
+}
+
+.ingredients__list {
+  @include clear-list;
+
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+.ingredients__item {
+  width: 100px;
+  min-height: 40px;
+  margin-right: 17px;
+  margin-bottom: 35px;
+}
+
+.ingredients__counter {
+  width: 54px;
+  margin-top: 10px;
+  margin-left: 36px;
+}
+
+.filling {
+  @include r-s14-h16;
+
+  position: relative;
+
+  display: block;
+
+  padding-left: 36px;
+
+  img {
+    @include p_center-v;
+
+    display: block;
+
+    width: 32px;
+    height: 32px;
+
+    box-sizing: border-box;
+    padding: 4px;
+
+    border-radius: 50%;
+  }
+}
+</style>
---
+++ frontend/src/modules/constructor/SauceComponent.vue
@@ -0,0 +1,175 @@
+<script setup>
+const props = defineProps({
+  sauceItems: {
+    type: Array,
+    required: true,
+  },
+});
+
+console.log(props.sauceItems);
+</script>
+
+<template>
+  <div class="ingredients__sauce">
+    <p>Основной соус:</p>
+
+    <label
+      v-for="(sauce, index) in sauceItems"
+      :key="sauce.id"
+      class="radio ingredients__input"
+    >
+      <input
+        type="radio"
+        name="sauce"
+        :value="sauce.value"
+        :checked="index === 0"
+      />
+      <span>{{ sauce.name }}</span>
+    </label>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/ds-system/ds.scss";
+@import "@/assets/scss/mixins/mixins.scss";
+
+.ingredients__sauce {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  width: 100%;
+  margin-bottom: 14px;
+
+  p {
+    @include r-s16-h19;
+
+    margin-top: 0;
+    margin-right: 16px;
+    margin-bottom: 10px;
+  }
+}
+
+.ingredients__input {
+  margin-right: 24px;
+  margin-bottom: 10px;
+}
+
+.input {
+  display: block;
+
+  span {
+    @include r-s14-h16;
+
+    display: block;
+
+    margin-bottom: 4px;
+  }
+
+  input {
+    @include r-s16-h19;
+
+    display: block;
+
+    box-sizing: border-box;
+    width: 100%;
+    margin: 0;
+    padding: 8px 16px;
+
+    transition: 0.3s;
+
+    color: $black;
+    border: 1px solid $purple-400;
+    border-radius: 8px;
+    outline: none;
+    background-color: $white;
+
+    font-family: inherit;
+
+    &:focus {
+      border-color: $green-500;
+    }
+  }
+
+  &:hover {
+    input {
+      border-color: $black;
+    }
+  }
+
+  &--big-label {
+    display: flex;
+    align-items: center;
+
+    span {
+      @include b-s16-h19;
+
+      margin-right: 16px;
+
+      white-space: nowrap;
+    }
+  }
+}
+
+.radio {
+  cursor: pointer;
+
+  span {
+    @include r-s16-h19;
+
+    position: relative;
+
+    padding-left: 28px;
+
+    &:before {
+      @include p_center-v;
+
+      display: block;
+
+      box-sizing: border-box;
+      width: 20px;
+      height: 20px;
+
+      content: "";
+      transition: 0.3s;
+
+      border: 1px solid $purple-400;
+      border-radius: 50%;
+      background-color: $white;
+    }
+  }
+
+  &:hover {
+    input:not(:checked):not(:disabled) + span {
+      &:before {
+        border-color: $purple-800;
+      }
+    }
+  }
+
+  input {
+    display: none;
+
+    &:checked + span {
+      &:before {
+        border: 6px solid $green-500;
+      }
+    }
+
+    &:disabled {
+      & + span {
+        &:before {
+          border-color: $purple-400;
+          background-color: $silver-200;
+        }
+      }
+
+      &:checked + span {
+        &:before {
+          border: 6px solid $purple-400;
+        }
+      }
+    }
+  }
+}
+</style>
